1a5adfefc9add46adc484d631af3c1a421ab7f77
Byndum <Mikael.List-Jensen@hotmail.dk>
2024-05-01 09:00:34 +0200

    re-added confirm and cancel button

--- src/main/java/com/acreservationsystem/model/AutocamperDAO.java
+++ src/main/java/com/acreservationsystem/model/AutocamperDAO.java
@@ -1,19 +1,12 @@
 package com.acreservationsystem.model;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
-
 
 public interface AutocamperDAO {
     public void rent(Autocamper ac);
     public ResultSet read(int no) throws SQLException;
-
     public ResultSet readAll() throws SQLException;
     public int readPrice(int no) throws SQLException;
-
     public void update (int no, Autocamper ac);
-
     public boolean delete (int no);
-
-
 }
